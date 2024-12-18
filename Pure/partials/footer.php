04b566de42d086826811bb5dbe806fa7fe9df6c1
webphone <minhkhuyen1711@gmail.com>
2024-12-19 00:22:23 +0700

    bt project

---
+++ Pure/partials/footer.php
@@ -0,0 +1,10 @@
+ <!-- cuoi -->
+<div class="header">
+    <div class="header-container">
+        <header class="header-bottom">
+            <span>2024 © HoangTuanAnh Academy. All rights reserved. Developed by TApro Software. 
+            </span>
+        </header>
+    </div>
+    <div class="container-botom"></div>
+</div>
